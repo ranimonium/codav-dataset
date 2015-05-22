868d6bbd021990d3930524914453619b8f7739dc
Jadurani Davalos <ranimonium@gmail.com>
2015-05-22 12:59:27 +0800

    PDF ready to graph Remaining reqt:  facility to actually test the pdf

---
+++ data-pdf/grapher.py
@@ -0,0 +1,77 @@
+import matplotlib as mpl
+mpl.use('TkAgg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+from user1.pdfdata import *
+user = 1
+
+
+
+weekday_dir = [
+    "Monday", 
+    "Tuesday", 
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday"
+    ]
+
+feature_data = {
+    "appname" : [  "Applications",  appname_max_per_day,  appname_week_data  ],
+    "phonecalling" : [  "Outgoing Calls",  phonecalling_max_per_day,  phonecalling_week_data  ],
+    "phoneringing" : [  "Incoming Calls",  appname_max_per_day,  appname_week_data  ],
+    "powerbattery" : [  "Battery Level",  powerbattery_max_per_day,  powerbattery_week_data  ],
+    "powercharger" : [  "Battery Charger",  powercharger_max_per_day,  powercharger_week_data  ],
+    "smsreceived" : [  "SMS Received",  smsreceived_max_per_day,  smsreceived_week_data  ],
+    "smssent" : [  "SMS Sent",  smssent_max_per_day,  smssent_week_data  ],
+    "wificonnected" : [   "BSSID",  wificonnected_max_per_day,  wificonnected_week_data  ],
+}
+
+
+def plot3dbar(user, day, day_data, filename, m):
+    
+    xpos, ypos, zpos = [], [], []
+    dz = []
+
+    for hourlydata in range(len(day_data)):
+        for value in day_data[hourlydata]:
+            xpos.append(0 + hourlydata)
+            ypos.append(day_data[hourlydata].index(value))
+            zpos.append(0)
+            dz.append(value)
+
+
+    dx = np.ones(len(xpos))
+    dy = np.ones(len(ypos))
+    # print dx, dy, dz
+
+    nrm=mpl.colors.Normalize(0,m)
+    colors=cm.jet(nrm(dz))
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    # ax = Axes3D(fig, azim=45,elev=15)
+    # ax = fig.add_subplot(111, projection='3d')
+
+    plt.ylabel(feature_data[filename][0])
+    plt.xlabel('Time of the Day (Hours)')
+    plt.title(feature_data[filename][0] + " on " + day.capitalize() + " - User " + str(user))
+
+    cax,kw=mpl.colorbar.make_axes(ax,shrink=.75,pad=.02) #add colorbar with normalized range
+    cb1=mpl.colorbar.ColorbarBase(cax,cmap=cm.jet,norm=nrm)
+    
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=colors)
+    plt.show()
+
+plot3dbar(user, 'Monday', feature_data['phonecalling'][2]['Monday'], 'phonecalling', feature_data['phonecalling'][1][0])
+# plot3dbar(user, 'Monday', data, 'phonecalling', feature_data['phonecalling'][1][0])
+
+for day in weekday_dir:
+    for feature in feature_data:
+        plot3dbar(user, day, feature_data[feature][2][day], feature, feature_data[feature][1][0])
--- pdfer.py
+++ pdfer.py
@@ -1,10 +1,16 @@
 import os
 import copy
 
-import matplotlib
-matplotlib.use('TkAgg')
+import matplotlib as mpl
+mpl.use('TkAgg')
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+# from weekdata import *
 
 cwd = os.getcwd()
 
@@ -22,87 +28,143 @@ def appname(type, line, week_data, directory):
                 # add a  0 to allow space for the frequency of the name
                 week_data[day][h].append(0)
 
-    # then we increment it by one
-    # print week_data
-    # print int(line[2])
-    # print weekday_dir[ int(line[2]) ]
-    # print line[3]
-    # print directory.index(line[6])
-    # print week_data[ weekday_dir[int(line[2])] ][int(line[3])]
-    # print week_data[ weekday_dir[int(line[2])] ][int(line[3])][ directory.index(line[6]) ]
     week_data[ weekday_dir[int(line[2])] ][int(line[3])][ directory.index(line[6]) ] += 1
 
 
 def phone(type, line, week_data, directory):
-    pass
+    appname(type, line, week_data, directory)
 
 def power(type, line, week_data, directory):
-    pass
+    appname(type, line, week_data, directory)
 
 def sms(type, line, week_data, directory):
-    pass
+    appname(type, line, week_data, directory)
 
 def wificonnected(type, line, week_data, directory):
-    pass
-
-
-
-
-
+    appname(type, line, week_data, directory)
 
 # timestamp --> month day weekday hours minutes seconds
-
 fcns = {
-    "app-name" : appname, # timestamp appname
-    # "phone-calling" : phone, # timestamp phonenumber
-    # "phone-ringing" : phone, # timestamp phonenumber
-    # "power-battery" : power, # timestamp batterylevel
-    # "power-charger" : power, # timestamp sourcetype
-    # "sms-received" : sms, # timestamp phonenumber
-    # "sms-sent" : sms, # timestamp phonenumber
-    # "wifi-connected" : wificonnected, # timestamp bssid
+    "app-name" :    [appname, "Applications"], # timestamp appname
+    "phone-calling" : [phone, "Outgoing Calls"], # timestamp phonenumber
+    "phone-ringing" : [phone, "Incoming Calls"], # timestamp phonenumber
+    "power-battery" : [power, "Battery Level"], # timestamp batterylevel
+    "power-charger" : [power, "Battery Level"], # timestamp sourcetype
+    "sms-received" : [sms, "SMS Received"], # timestamp phonenumber
+    "sms-sent" : [sms, "SMS Sent"], # timestamp phonenumber
+    "wifi-connected" : [wificonnected, "BSSID"], # timestamp bssid
     }
 
 weekday_dir = [
-    "monday", "tuesday", "wednesday",
-    "thursday", "friday", "saturday", "sunday"
+    "Monday", 
+    "Tuesday", 
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday"
     ]
 
 
+def plot3dbar(user, day, day_data, filename, m):
+    
+    xpos, ypos, zpos = [], [], []
 
-for user in range (1,7):
+    dx = np.ones(len(xpos))
+    dy = np.ones(len(ypos))
+    dz = []
 
-    # stores your matrices
-    week_data = {}
-    for day in weekday_dir:
-        # stores your matrix for a single day
-        # this list contains empty lists that will soon store your 
-        # frequency distribution per name
-        week_data[day] = [[] for i in range(24)] 
+    for hourlydata in range(len(day_data)):
+        for value in day_data[hourlydata]:
+            xpos.append(0 + hourlydata)
+            ypos.append(day_data[hourlydata].index(value))
+            zpos.append(0)
+            dz.append(value)
+
+    nrm=mpl.colors.Normalize(0,m)
+    colors=cm.jet(nrm(dz))
+
+    fig = plt.figure()
+    ax = Axes3D(fig, azim=45,elev=15)
+    # ax = fig.add_subplot(111, projection='3d')
+
+    plt.ylabel(fcns[filename][1])
+    plt.xlabel('Time of the Day (Hours)')
+    plt.title(fcns[filename][1] + " on " + day.capitalize() + " - User " + str(user))
+
+    cax,kw=mpl.colorbar.make_axes(ax,shrink=.75,pad=.02) #add colorbar with normalized range
+    cb1=mpl.colorbar.ColorbarBase(cax,cmap=cm.jet,norm=nrm)
+    
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=colors)
+    plt.show()
 
 
-    # stores contact numbers, ssid, battery level?, app names
-    directory = []
+
+for user in range (2,7):
+
 
 
     for filename in fcns.keys():
+    # filename = "app-name"
+    # if filename == "app-name":
 
+        # stores your matrices
+        week_data = {}
+        for day in weekday_dir:
+            # stores your matrix for a single day
+            # this list contains empty lists that will soon store your 
+            # frequency distribution per name
+            week_data[day] = [[] for i in range(24)] 
+
+
+        # stores contact numbers, ssid, battery level?, app names
+        directory = []
         #### READ DATA ####
         rawdata = [(line.strip()).split(" ") for line in open(cwd + 
             "/data-parsed-pdf/" + str(user) + "/" + filename + ".ssv")]
 
-        # try:
-
+        try:
         #### POPULATE DISTRIBUTION FCN ####
-        for line in rawdata:
-            fcns[filename](filename.split('-')[1], line, week_data, directory)
-        # except Exception, e:
-            # print line, e
+            for line in rawdata:
+                if "(invalid" in line:
+                    break
+                fcns[filename][0](filename.split('-')[1], line, week_data, directory)
 
-        print week_data
-        
-        #### PLOT 3D BAR GRAPH ####
-        
+        except Exception, e:
+            print line, e
 
+        # print week_data
+        
+        # max_per_day = []
+        # #### NORMALIZE AND PLOT ####
+        # for day in week_data:
+        #     s = 0
+        #     for d in week_data[day]:
+        #         s += sum(d)
+
+        #     if s == 0:
+        #         print("S IS 0 -- " + str(user) + " " + filename + " " + day)
+        #         s = 1
+            
+        #     m = 0
+        #     for hourlydata in week_data[day]:
+        #         for i in range(len(hourlydata)):
+        #             # print hourlydata[i]
+        #             hourlydata[i] = float(hourlydata[i])/s
+        #         if max(hourlydata) > m:
+        #             m = max(hourlydata)
+        #             # print m
+
+        #     max_per_day.append(m)
+            
+            ###  PLOT 3D BAR GRAPH  ###
+            # plot3dbar(user, day, week_data[day], filename, m)
+        print(str(user) + " print directory " + filename)
+
+        pdfile = open(cwd + "/data-pdf/user" + str(user) + "/pdfdata.py", "a")
+        # pdfile.write("max_per_day = " + str(max_per_day) + "\n")
+        # pdfile.write("week_data = " + str(week_data) + "\n\n")
+        pdfile.write( "\n" + "".join(filename.split('-')) + "_directory = " + str(directory) + "\n")
+        pdfile.close()
         
 plt.close()
