815fc1d584c4ff0efa3ecaa01854b86c1516e3d8
Jadurani Davalos <ranimonium@gmail.com>
2015-05-25 16:10:05 +0800

    Generates data that records merely the presence of an event given a feature value, day, and time

--- data-pdf-undivided/gen.py
+++ data-pdf-undivided/gen.py
@@ -23,21 +23,44 @@ weekday_dir = [
     ]
 
 # for phonecalls and sms only
+def pdfmaxerror():
+    testdata = []
+    for u in range(1,7):
+        for feature in feature_dir:
+            maxinday = max(motherpdf['user' + str(u)][feature][3])
+            indexofmaxinday =  motherpdf['user' + str(u)][feature][3].index(maxinday) #day
+            day = weekday_dir[indexofmaxinday]
+
+            for hour in range(24):
+                if maxinday in motherpdf['user' + str(u)][feature][1][day][hour]:
+                    indexof_max_featurevalue = motherpdf['user' + str(u)][feature][1][day][hour].index(maxinday)
+                    feature_value = motherpdf['user' + str(u)][feature][2][indexof_max_featurevalue]
+                    for i in range(random.randint(1, maxinday)):
+                    # for i in range(maxinday):
+                        testdata.append(('user' + str(u), feature, day, hour, feature_value))
+                    break
+
+
+    # print testdata
 testdata = []
-for u in range(1,7):
-    for feature in feature_dir:
-        maxinday = max(motherpdf['user' + str(u)][feature][3])
-        indexofmaxinday =  motherpdf['user' + str(u)][feature][3].index(maxinday) #day
-        day = weekday_dir[indexofmaxinday]
 
-        for hour in range(24):
-            if maxinday in motherpdf['user' + str(u)][feature][1][day][hour]:
-                indexof_max_featurevalue = motherpdf['user' + str(u)][feature][1][day][hour].index(maxinday)
-                feature_value = motherpdf['user' + str(u)][feature][2][indexof_max_featurevalue]
-                for i in range(random.randint(1, maxinday)):
-                # for i in range(maxinday):
-                    testdata.append(('user' + str(u), feature, day, hour, feature_value))
-                break
+def pdfchoice2():
+    # generate randomly from the motherpdf
+    # we could go convert the whole data for a day to a presence or absence shit
+    # then we could convert the shit
+    for u in range(1,7):
+        for feature in feature_dir:
+            for day in weekday_dir:
+                for hour in range(24):
+                    valuelistinhour = motherpdf['user' + str(u)][feature][1][day][hour]
+                    # print valuelistinhour
 
+                    indices_nonzeroelements = [i for i, e in enumerate(valuelistinhour) if e != 0]
+                    # print indices_nonzeroelements
 
-# print testdata
+                    for index in indices_nonzeroelements:
+                        feature_value = motherpdf['user' + str(u)][feature][2][index]
+                        testdata.append(('user' + str(u), feature, day, hour, feature_value))
+
+pdfchoice2()
+# print "testdata \n\n", testdata, "\n\n"
